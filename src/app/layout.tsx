1a087dc9068a28f8468841f11808f7a62db4032f
shakedshoshan <shakedshoshan8@gmail.com>
2024-06-22 19:45:19 +0300

    setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import Header from "@/components/Header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+//import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* Toaster */}
+
+
+      {/* Header */}
+      <header>
+        <Header />
+      </header>
+
+      <div>
+         <body className={inter.className}>{children}</body>
+      </div>
     </html>
   );
 }
